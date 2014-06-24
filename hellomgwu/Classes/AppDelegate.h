046afa9e068c36af8d1b3218234b2a437a4fdc94
PHENOM <devondaphenom@Devons-MacBook-Pro.local>
2014-06-24 08:39:23 -0400

    Initial Commit

---
+++ hellomgwu/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  hellomgwu
+//
+//  Created by PHENOM on 6/24/14.
+//  Copyright ___mgwu___ 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ hellomgwu/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hellomgwu
+//
+//  Created by PHENOM on 6/24/14.
+//  Copyright ___mgwu___ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
